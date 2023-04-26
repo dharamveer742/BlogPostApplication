a88458c37da6fc586791e92180045a183a1d232c
dharamveer <dharamveer.gla_cs20@gla.ac.in>
2023-04-26 16:55:53 +0530

    added route for logout

--- BlogAppAPI/index.js
+++ BlogAppAPI/index.js
@@ -2,46 +2,46 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const authRoute  = require("./routes/auth");
-const usersRoute = require("./routes/users");
-const postsRoute = require("./routes/posts");
-const categoriesRoute = require("./routes/categories")
-
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
 const multer = require("multer");
-
-
-
-app.use(express.json());
+const path = require("path");
 
 dotenv.config();
+app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
 
-mongoose.connect(process.env.MONGO_URL,{
-    useUnifiedTopology:true,
-    useNewUrlParser:true,
-   
-}).then(console.log("connected to mongo db")).catch((err)=> console.log(err));
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    
+    
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
-const storage =  multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null,"images")
-    },filename:(req,file,cb)=>{
-        cb(null,req.body.name);
-    }
-})
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
 
-const upload = multer({storage:storage})
-app.post("/upload",upload.single("file"),(req,res)=>{
-    res.status(200).json("file has been uploaded")
-})
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
 
-app.use("/auth",authRoute);
-app.use("/users",usersRoute);
-app.use("/posts",postsRoute);
-app.use("/categories",categoriesRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
-app.use("/",(req,res)=>{
-    console.log("mainUrl"); 
-})
-app.listen("5000",()=>{
-    console.log("running");
-})
+app.listen("5000", () => {
+  console.log("Backend is running.");
+});
